87ef2d583836c412e7b51eec9a1d58627ade25ae
Developer Hasan <67751842+DeveloperHasanBD@users.noreply.github.com>
2022-06-29 14:15:17 +0600

    Update index.php

--- index.php
+++ index.php
@@ -23,22 +23,6 @@ function mrs_orl_sec_2_backend()
                 ),
 
                 array(
-                    "type" => "textfield",
-                    "holder" => "img",
-                    "class" => "",
-                    "heading" => __("Title", "mersmann"),
-                    "param_name" => "mrs_orl_sec_2_title",
-                    "value" => __("", "mersmann"),
-                ),
-                array(
-                    "type" => "textarea",
-                    "holder" => "img",
-                    "class" => "",
-                    "heading" => __("Description", "mersmann"),
-                    "param_name" => "mrs_orl_sec_2_description",
-                    "value" => __("", "mersmann"),
-                ),
-                array(
                     'type' => 'param_group',
                     'param_name' => 'mrs_orl_sec_2_items',
                     'params' => array(
@@ -48,31 +32,6 @@ function mrs_orl_sec_2_backend()
                             "class" => "",
                             "heading" => __("Small Image", "mersmann"),
                             "param_name" => "mrs_orl_sec_2_sml_img",
-                            "value" => __("", "mersmann"),
-                        ),
-
-                        array(
-                            "type" => "attach_image",
-                            "holder" => "img",
-                            "class" => "",
-                            "heading" => __("If you have different logo for mobile, pls insert or make it blank", "mersmann"),
-                            "param_name" => "mrs_orl_sec_2_mbl_logo_img",
-                            "value" => __("", "mersmann"),
-                        ),
-                        array(
-                            "type" => "textfield",
-                            "holder" => "",
-                            "class" => "",
-                            "heading" => __("Small title", "mersmann"),
-                            "param_name" => "mrs_orl_sec_2_sml_title",
-                            "value" => __("", "mersmann"),
-                        ),
-                        array(
-                            "type" => "vc_link",
-                            "holder" => "",
-                            "class" => "",
-                            "heading" => __("Url", "mersmann"),
-                            "param_name" => "mrs_orl_sec_2_sml_url",
                             "value" => __("", "mersmann"),
                         ),
 
@@ -115,14 +74,6 @@ function mrs_orl_sec_2_backend()
                     )
                 ),
                 array(
-                    "type" => "attach_images",
-                    "holder" => "img",
-                    "class" => "",
-                    "heading" => __("Slider Images", "mersmann"),
-                    "param_name" => "mrs_orl_sec_2_rt_slider_img",
-                    "value" => __("", "mersmann"),
-                ),
-                array(
                     "type" => "textarea_raw_html",
                     "holder" => "img",
                     "class" => "",
@@ -143,19 +94,19 @@ function mrs_orl_sec_2_view($atts)
     ob_start();
     $atts = shortcode_atts(array(
         'mrs_orl_sec_2_logo_img' => '',
-        'mrs_orl_sec_2_title' => '',
-        'mrs_orl_sec_2_description' => '',
         'mrs_orl_sec_2_items' => '',
-        'mrs_orl_sec_2_rt_slider_img' => '',
         'mrs_orl_sec_2_ins_ifcode' => '',
     ), $atts, 'mrs_orl_sec_2');
 
     $mrs_orl_sec_2_logo_img_id = $atts['mrs_orl_sec_2_logo_img'] ?? '';
     $mrs_orl_sec_2_logo_img_url = wp_get_attachment_image_url($mrs_orl_sec_2_logo_img_id, 'full');
     $items = vc_param_group_parse_atts($atts['mrs_orl_sec_2_items']);
-    $mrs_orl_sec_2_ins_ifcode = rawurldecode(base64_decode($atts['mrs_orl_sec_2_ins_ifcode']));;
-    $is_mrs_orl_sec_2_sml_snd_url = vc_build_link($mrs_orl_sec_2_sml_snd_url);
-    $mrs_orl_sec_2_sml_snd_url_set = $is_mrs_orl_sec_2_sml_snd_url['url'];
+    $mrs_orl_sec_2_ins_ifcode = rawurldecode(base64_decode($atts['mrs_orl_sec_2_ins_ifcode']));
+
+    // $mrs_orl_sec_2_sml_url = $single_item['mrs_orl_sec_2_sml_url'] ?? '';
+    // $is_mrs_orl_sec_2_sml_url = vc_build_link($mrs_orl_sec_2_sml_url) ?? '';
+    // $mrs_orl_sec_2_sml_url_set = $is_mrs_orl_sec_2_sml_url['url'];
+    
 
 ?>
     <!-- html code here  -->
